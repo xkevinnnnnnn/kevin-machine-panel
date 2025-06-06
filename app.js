4d8e5d2ed1806f31e84c801c200519a767f59546
Developer Kevin <angelpereira46731@.com>
2025-06-06 08:41:43 -0300

    Actualización: nuevo index.html y style.css

--- app.js
+++ app.js
@@ -38,4 +38,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor iniciado en http://localhost:${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor iniciado en el puerto ${PORT}`);
+});
